beb4e3229fa4341898ddcd3e79a30dd1940513f8
John Lira <johnalsoares@gmail.com>
2026-01-18 20:53:03 -0300

    Add custom Cypress commands for adding items to cart and verifying cart contents

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -7,3 +7,17 @@ Cypress.Commands.add('login', (username, password)=>{
     cy.fillInput('password', password)
     cy.get('[data-test="login-button"]').click();
 })
+
+Cypress.Commands.add('addToCart',(n)=>{
+    return cy.get('[data-test="inventory-item"]').eq(n).then(($item) => {
+        const productName = Cypress.$($item).find('[data-test="inventory-item-name"]').text().trim()
+        return cy.wrap($item).find('[data-test^="add-to-cart"]').click().then(() => {
+            return productName
+        })
+   })
+})
+
+Cypress.Commands.add('verifyCart', (selectedProducts)=>{
+    cy.get('[data-test="shopping-cart-link"]').click();
+    selectedProducts.forEach((product) => cy.contains(product))
+})
